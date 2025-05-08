69167fd85762bc1a24b64a981aa5a265b8d35c9f
mehdi220320 <111698017+mehdi220320@users.noreply.github.com>
2025-05-08 03:56:06 +0100

    admin component

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {UserlistComponent} from './userlist/userlist.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'users', component: UserlistComponent },
 
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
